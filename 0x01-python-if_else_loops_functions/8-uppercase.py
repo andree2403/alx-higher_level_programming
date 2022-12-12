2a907526d1c11414f7b834cbf3d4d34bfde85bad
andree2403 <cjmorgan2589@gmail.com>
2022-12-12 17:13:45 +0000

    Tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(str):
-    str = ord(str)
-    for str in range(65, 123):
-        if str >= 65 and str < 91:
-            print({}.format(str))
+    for i in str:
+        i = ord(i)
+        if i >= 65 and i < 91:
+            print({}.format(str), end="")
         else:
             continue
 
