a6963eb3842230e709eb3463d487fb1aed67aaa1
andree2403 <cjmorgan2589@gmail.com>
2022-12-14 11:33:28 +0000

    Tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    int result == 0
+    for i in range(count):
+        int(sys.argv[i])
+        result = result + sys.argv[i]
+
