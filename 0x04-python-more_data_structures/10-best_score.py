586d9cf1b0064311504c50fade89fc5a0ee5396a
andree2403 <cjmorgan2589@gmail.com>
2023-01-20 16:13:03 +0000

    Task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary or len(a_dictionary) == 0:
         return None
-    best_key = max(a_dictionary, key = a_dictionary.get)
+    best_key = max(a_dictionary, key=a_dictionary.get)
     return best_key
