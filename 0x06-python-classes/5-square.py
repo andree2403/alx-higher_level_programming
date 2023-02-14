5491ebc5340961b2b4e835e795a57d82603add95
andree2403 <cjmorgan2589@gmail.com>
2023-02-14 22:16:28 +0000

    Tasks

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size=0):
+        """Private instance attribute with size"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the area of the square"""
+        return "{}".format(self.__size * self.__size)
+
+    @property
+    def size(self):
+        """For retrieving the value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """For setting the value for the size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+        def my_print(self):
+            """prints in stdout the square with the character #"""
+            if self.__size == 0:
+                print('')
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
