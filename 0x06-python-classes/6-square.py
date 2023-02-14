830e3a6623763877ea2371121038a2af10fab02a
andree2403 <cjmorgan2589@gmail.com>
2023-02-14 23:09:43 +0000

    Tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,14 +32,15 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        """prints in stdout the square with the character #"""
+        """Prints a square."""
         if self.__size == 0:
             print('')
+            return
         for i in range(self.__position[1]):
             print('')
         for i in range(self.__size):
             for k in range(self.__position[0]):
-                print(" ", end="")
+                print(' ', end='')
             for j in range(self.__size):
                 print('#', end="")
             print()
