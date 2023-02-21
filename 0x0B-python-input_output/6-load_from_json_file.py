87243abae4e0cb16286f77e265d09ef6da333f07
andree2403 <cjmorgan2589@gmail.com>
2023-02-21 22:36:21 +0000

    Tasks

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a JSON file-writing function."""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an object from a json file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        x = json.load(f)
