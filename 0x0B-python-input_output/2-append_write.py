b751a83317defb96105a1efda2d26cdb8fe1451e
andree2403 <cjmorgan2589@gmail.com>
2023-02-21 20:33:15 +0000

    Tasks

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a text to a file"""
+    with open('workfile', 'a', encoding="utf-8") as g:
+        return g.write(text)
