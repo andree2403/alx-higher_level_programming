0fb0f9f71577a9a0beee1a762069fada241ebe7e
andree2403 <cjmorgan2589@gmail.com>
2023-02-21 02:03:31 +0000

    Tasks

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,18 +2,8 @@
 """Defines an inherited class-checking function."""
 
 
-class BaseGeometry:
-    """An empty class BaseGeometry"""
-    def area(self):
-        """raise exception with a message"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """public instance method def integer validator"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""rectangle module"""
 
 class Rectangle(BaseGeometry):
     """a subclass named rectangle"""
