0ef227028835f6f0d67afa2ea27dd966e6348af4
andree2403 <cjmorgan2589@gmail.com>
2023-02-21 02:41:32 +0000

    Tasks

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""creation of square class to inherit from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""imported module"""
+
+
+class Square(Rectangle):
+    """creatng a class names Square"""
+    def __init__(self, size):
+        """instalizing the Square class"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area must be implemented"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string format to print"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
