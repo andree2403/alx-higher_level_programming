02f2abdf21ce6c7aec2b491a6f222b97f727fd38
andree2403 <cjmorgan2589@gmail.com>
2023-02-23 08:50:09 +0000

    Tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Adds all argument to a python list and saves it to file"""
 
+
 import sys
-"""imported module"""
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
