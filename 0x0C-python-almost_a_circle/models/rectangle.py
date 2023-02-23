82d2c59f62f71679eb61b6ecd2035cac5a853c7a
andree2403 <cjmorgan2589@gmail.com>
2023-02-23 22:24:31 +0000

    Tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,4 +87,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns the representation of the answer"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.__x, self.__y, self.__width, self.__height)
