46b71387803e244d96f58b024580dec836ad11f5
andree2403 <cjmorgan2589@gmail.com>
2023-02-23 17:39:53 +0000

    Tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,10 @@
 class Base:
     """creste a class named base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """class constructor"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
