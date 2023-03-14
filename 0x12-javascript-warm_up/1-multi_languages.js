6e7423c4f04eb94a38eae7a4c59f38d321e65174
andree2403 <cjmorgan2589@gmail.com>
2023-03-14 11:02:53 -0700

    Tasks

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myfirst = 'C is fun';
+const mysecond = 'Python is cool';
+const mythird = 'JavaScript is amazing';
+
+console.log(myfirst);
+console.log(mysecond);
+console.log(mythird);
