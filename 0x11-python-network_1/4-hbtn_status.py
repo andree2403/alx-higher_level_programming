eaab24559394f407202518ef1972cc11be326928
andree2403 <cjmorgan2589@gmail.com>
2023-05-10 20:37:21 +0000

    Task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
-
+"""Takes URL and prints out response"""
 import requests
-
 if __name__ == "__main__":
-    response = requests.get('https://httpbin.org/basic-auth/user/pass')
+    rep = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(rep.text)))
+    print("\t- content: {}".format(rep.text))
---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get("X-Request-Id"))
