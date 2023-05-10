4fdd26c53424fa1860ca4617ddb0aa6e4e5751b6
andree2403 <cjmorgan2589@gmail.com>
2023-05-10 20:23:43 +0000

    Task

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://httpbin.org/basic-auth/user/pass')
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
