df0771aa6311cf6d3170c378bdeef4a739d94be0
andree2403 <cjmorgan2589@gmail.com>
2023-07-03 22:42:14 +0000

    Task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,25 @@
 
 const request = require('request');
 const url = process.argv[2];
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    const obj = {};
-    body = JSON.parse(body);
-    for (let i = 0; i < body.length; ++i) {
-      if (body[i].completed && !obj[body[i].userId]) {
-        obj[body[i].userId] = 0;
-      }
-      if (body[i].completed) {
-        obj[body[i].userId]++;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-    console.log(obj);
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
